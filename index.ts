4600ff50019ae68e9ed41eaf0a0c14f48cc16654
DaviRKL <davirkl07@gmail.com>
2024-04-20 22:37:41 -0300

    Alterei o cors para que ele aceite o get da porta 3000

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import cors from "cors";
 import userRoutes from "./routes/users";
 
 const app = express();
-
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/", userRoutes);
 
